0e52ec49e4ee89860b674337034af874eadcb0d9
zhangchangsheng <414052147@qq.com>
2025-08-30 17:17:06 +1200

    update zoo

--- Week_5/Zoo.py
+++ Week_5/Zoo.py
@@ -4,28 +4,46 @@
 
 class Color:
     name:str
-    def __init__(self,name):
+    red:int
+    green:int
+    blue:int
+
+    def __init__(self,name:str,red:int,green:int,blue:int):
         self.name = name 
+        self.red = red
+        self.blue = blue
+        self.green = green
+
 
     def show_color(self):
-        print(f"{self.name}")
+        format = f"{self.name}, r:{self.red},g:{self.green},b:{self.blue}"
+        print(format)
+        return format
         
 
 
 class TransparentColor(Color):
+    transparent:int
 
-    def __init__(self,name):
-        super().__init__(name)
+    def __init__(self,name,red,green,blue,transparent:int):
+        super().__init__(name,red,green,blue)
+        self.transparent = transparent
 
 
     def show_color(self):
-        print(f"transparent")
+        normal_format = super().show_color()
+        format = normal_format + f",transparent:{self.transparent}"
+        print(format)
+        return format
+
+
+
 
 
 
 def main():
-    red = Color("red")
-    transparent = TransparentColor("black")
+    red = Color("red",0xff,0x00,0x00)
+    transparent = TransparentColor("transparent",0xff,0x00,0x00,199)
 
     red.show_color()
     transparent.show_color()
