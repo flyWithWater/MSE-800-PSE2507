7a823314913438295abb836549ca753d8b33a667
Changsheng Zhang <makechange234@gmail.com>
2025-11-04 15:52:55 +1300

    caculate the execution time of a function through decorator of python

--- week_14/test_decorator.py
+++ week_14/test_decorator.py
@@ -1,18 +1,22 @@
 
-
-
-
-
-
 # this is a decorator of the function object
+import time
+
+
 def log_decorator(func):
     def wrapper(*args, **kwargs):
         #it is printing the original function's name and 
         print(f"Calling {func.__name__} with {args}, {kwargs}")
+        start_nanoseconds = int(round(time.time()*1000000))
+        print(f"start_millis:{start_nanoseconds}")
         # this is a call of the original function, and result are just the result we get from the original function
         result = func(*args, **kwargs)
+        end_nanoseconds = int(round(time.time()*1000000))
+
+        execution_nanoseconds = end_nanoseconds-start_nanoseconds
+
         #func is just the object of the function . the syntax is printing its name and actual result from it.
-        print(f"{func.__name__} returned {result}")
+        print(f"{func.__name__} executed {execution_nanoseconds} nanoseconds , returned {result}")
         return result
     return wrapper
 
