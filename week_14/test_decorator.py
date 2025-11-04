b085fc3f897c477ab3133e667cb3fa08765695fc
Changsheng Zhang <makechange234@gmail.com>
2025-11-04 14:49:36 +1300

    decorator

--- week_14/test_decorator.py
+++ week_14/test_decorator.py
@@ -4,11 +4,14 @@
 
 
 
-
+# this is a decorator of the function object
 def log_decorator(func):
     def wrapper(*args, **kwargs):
+        #it is printing the original function's name and 
         print(f"Calling {func.__name__} with {args}, {kwargs}")
+        # this is a call of the original function, and result are just the result we get from the original function
         result = func(*args, **kwargs)
+        #func is just the object of the function . the syntax is printing its name and actual result from it.
         print(f"{func.__name__} returned {result}")
         return result
     return wrapper
